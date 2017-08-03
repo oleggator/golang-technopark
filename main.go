3ac27a1728a0adb5aa9c9bc277cefff595cddafd
Oleg Utkin <oleg_utkin@me.com>
2017-08-03 03:05:59 +0300

    First commit

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+)
+
+const (
+	bufferSize         = 32 * 1024 // 32kb
+	separatorCharacter = '\n'      // newline character
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		executableName := os.Args[0]
+		fmt.Println("usage: " + executableName + " [FILE]")
+		return
+	}
+
+	filePath := os.Args[1]
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	linesCount, err := CountLinesInFile(file)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(linesCount)
+}
+
+func CountLinesInFile(source io.Reader) (int, error) {
+	buffer := make([]byte, bufferSize)
+	linesCount := 0
+	separator := []byte{separatorCharacter}
+
+	for {
+		charsCount, err := source.Read(buffer)
+		linesCount += bytes.Count(buffer[:charsCount], separator)
+
+		if err == io.EOF {
+			return linesCount, nil
+		} else if err != nil {
+			return linesCount, err
+		}
+	}
+}
